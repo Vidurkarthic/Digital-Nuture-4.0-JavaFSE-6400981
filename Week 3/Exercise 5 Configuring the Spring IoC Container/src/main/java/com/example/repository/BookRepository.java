90f21fca50e7ac44a98df96f4fd9730f15a221ea
Vidurkarthic <124781870+Vidurkarthic@users.noreply.github.com>
2025-07-06 16:00:47 +0530

    WEEK 3 UPDATED

---
+++ Week 3/Exercise 5 Configuring the Spring IoC Container/src/main/java/com/example/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+
+public class BookRepository {
+    public void getBooks() {
+        System.out.println("1. Java: The Complete Reference");
+        System.out.println("2. Effective Java");
+        System.out.println("3. Spring in Action");
+    }
+}
